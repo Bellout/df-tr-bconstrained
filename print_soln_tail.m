84617889c3e504875dc4b2e41494a7b9ed9c6fea
bellout <mathias.bellout@ntnu.no>
2018-12-06 10:29:09 +0100

    Scripts to print head and tail of data file.

---
+++ print_soln_tail.m
@@ -0,0 +1,9 @@
+fprintf(prob.fid, '%s\n', '');
+fprintf(prob.fid, '%s\n', '');
+fprintf(prob.fid, '%s\n', '        }');
+fprintf(prob.fid, '%s\n', '');
+fprintf(prob.fid, '%s\n', '    };');
+fprintf(prob.fid, '%s\n', '');
+fprintf(prob.fid, '%s\n', '}');
+fprintf(prob.fid, '%s\n', '');
+fprintf(prob.fid, '%s\n', '#endif //FIELDOPT_TEST_TR_DFO_DATA_H');
