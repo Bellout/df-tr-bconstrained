f89c7f7f43289ea5e1ca036308630b0d153d886a
bellout <mathias.bellout@ntnu.no>
2019-04-06 19:03:26 +0200

    Revert "Scripts to print head and tail of data file."
    This reverts commit 84617889c3e504875dc4b2e41494a7b9ed9c6fea.

--- print_soln_tail.m
+++
@@ -1,9 +0,0 @@
-fprintf(prob.fid, '%s\n', '');
-fprintf(prob.fid, '%s\n', '');
-fprintf(prob.fid, '%s\n', '        }');
-fprintf(prob.fid, '%s\n', '');
-fprintf(prob.fid, '%s\n', '    };');
-fprintf(prob.fid, '%s\n', '');
-fprintf(prob.fid, '%s\n', '}');
-fprintf(prob.fid, '%s\n', '');
-fprintf(prob.fid, '%s\n', '#endif //FIELDOPT_TEST_TR_DFO_DATA_H');
