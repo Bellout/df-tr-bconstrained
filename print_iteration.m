b3d0fdcf07af574daaee2139b4d459d0d5393ec0
bellout <mathias.bellout@ntnu.no>
2019-04-20 23:13:30 +0200

    Working code, checks against original.

--- print_iteration.m
+++ print_iteration.m
@@ -1,7 +1,6 @@
-function print_iteration(iter, fval, rho, radius, points)
+function print_iteration(iter, fval, rho, radius, points, prob)
 
     fprintf(1, '% 4d    % +10.3g    % 9.3g    % 9.3g    % 4d\n', iter, fval, rho, radius, points);
-
-
+    fprintf(prob.dbg_file_fid, '% 6d    % +10.3g    % 9.3g    % 9.3g    % 4d\n', iter, fval, rho, radius, points);
 
 end
--- tr_modeling/polynomials/evaluate_polynomial.m
+++ tr_modeling/polynomials/evaluate_polynomial.m
@@ -3,13 +3,8 @@ function [ value prob ] = evaluate_polynomial(polynomial, point, prob)
 
 [ nr nc ] = size(polynomial.coefficients);
 
-% ----------------------------------------------------------
-if (~strcmp(prob.cf_prev, prob.cf))
-  fprintf(prob.fidpc, '[ %s (%s)]\n', prob.cf, prob.cf_prev);
-end
-prob.cf_prev = prob.cf;
 % fprintf(prob.fidpc, [ repmat('%20.16f ', 1, nr) '\n'] , polynomial.coefficients);
-fprintf(prob.fidpc, [ repmat('%22.12e ', 1, nr) '\n'] , polynomial.coefficients);
+% fprintf(prob.fidpc, [ repmat('%22.12e ', 1, nr) '\n'] , polynomial.coefficients);
 
 % ----------------------------------------------------------
 [c, g, H] = coefficients_to_matrices(polynomial.dimension, ...
