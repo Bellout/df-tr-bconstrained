dd31f9bee86c4d583172d8b9c50dca8508dc5149
bellout <mathias.bellout@ntnu.no>
2019-11-27 08:54:51 +0100

    Dbg output files.

---
+++ trust_region_test/cp-focpp-prob-files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# COPY CG PROB FILES
+TRGDIR=~/Dropbox/SharedFolderTRDFO/reports/r01-2019-trdfo-benchmark/data/cg-probs
+FODIR=~/git/MB/FOEx/FOEx/cmake-build-debug/dbg/tr_dfo
+
+cp ${FODIR}/prob1/prob-soln-iters-bport-prob1_focpp.txt ${TRGDIR}/prob01/prob-soln-iters-bport-prob1-FOCPP.tex
+cp ${FODIR}/prob2/prob-soln-iters-bport-prob2_focpp.txt ${TRGDIR}/prob02/prob-soln-iters-bport-prob2-FOCPP.tex
+cp ${FODIR}/prob3/prob-soln-iters-bport-prob3_focpp.txt ${TRGDIR}/prob03/prob-soln-iters-bport-prob3-FOCPP.tex
+cp ${FODIR}/prob4/prob-soln-iters-bport-prob4_focpp.txt ${TRGDIR}/prob04/prob-soln-iters-bport-prob4-FOCPP.tex
+cp ${FODIR}/prob5/prob-soln-iters-bport-prob5_focpp.txt ${TRGDIR}/prob05/prob-soln-iters-bport-prob5-FOCPP.tex
+cp ${FODIR}/prob6/prob-soln-iters-bport-prob6_focpp.txt ${TRGDIR}/prob06/prob-soln-iters-bport-prob6-FOCPP.tex
+cp ${FODIR}/prob7/prob-soln-iters-bport-prob7_focpp.txt ${TRGDIR}/prob07/prob-soln-iters-bport-prob7-FOCPP.tex
+cp ${FODIR}/prob8/prob-soln-iters-bport-prob8_focpp.txt ${TRGDIR}/prob08/prob-soln-iters-bport-prob8-FOCPP.tex
+cp ${FODIR}/prob9/prob-soln-iters-bport-prob9_focpp.txt ${TRGDIR}/prob09/prob-soln-iters-bport-prob9-FOCPP.tex
+cp ${FODIR}/prob10/prob-soln-iters-bport-prob10_focpp.txt ${TRGDIR}/prob10/prob-soln-iters-bport-prob10-FOCPP.tex
+cp ${FODIR}/prob11/prob-soln-iters-bport-prob11_focpp.txt ${TRGDIR}/prob11/prob-soln-iters-bport-prob11-FOCPP.tex
---
+++ trust_region_test/cp-matlab-prob-files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# COPY CG PROB FILES
+TRGDIR=~/Dropbox/SharedFolderTRDFO/reports/r01-2019-trdfo-benchmark/data/cg-probs
+CGDIR=~/git/MB/df-tr-bconstrained/trust_region_test
+
+cp ${CGDIR}/prob-soln-iters-bport-prob1-MATLAB.tex ${TRGDIR}/prob01
+cp ${CGDIR}/prob-soln-iters-bport-prob2-MATLAB.tex ${TRGDIR}/prob02
+cp ${CGDIR}/prob-soln-iters-bport-prob3-MATLAB.tex ${TRGDIR}/prob03
+cp ${CGDIR}/prob-soln-iters-bport-prob4-MATLAB.tex ${TRGDIR}/prob04
+cp ${CGDIR}/prob-soln-iters-bport-prob5-MATLAB.tex ${TRGDIR}/prob05
+cp ${CGDIR}/prob-soln-iters-bport-prob6-MATLAB.tex ${TRGDIR}/prob06
+cp ${CGDIR}/prob-soln-iters-bport-prob7-MATLAB.tex ${TRGDIR}/prob07
+cp ${CGDIR}/prob-soln-iters-bport-prob8-MATLAB.tex ${TRGDIR}/prob08
+cp ${CGDIR}/prob-soln-iters-bport-prob9-MATLAB.tex ${TRGDIR}/prob09
+cp ${CGDIR}/prob-soln-iters-bport-prob10-MATLAB.tex ${TRGDIR}/prob10
+cp ${CGDIR}/prob-soln-iters-bport-prob11-MATLAB.tex ${TRGDIR}/prob11
